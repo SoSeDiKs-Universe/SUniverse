01c9e80d0f9a32f651c639babf8b527c8279dc5a
SoSeDiK <mrsosedik@gmail.com>
2024-06-23 17:13:42 +0300

    Fix wrong slot mimiking

--- Requiem/src/main/java/me/sosedik/requiem/listener/player/PossessedMimikPossessor.java
+++ Requiem/src/main/java/me/sosedik/requiem/listener/player/PossessedMimikPossessor.java
@@ -4,7 +4,6 @@ import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import com.google.common.base.Function;
 import io.papermc.paper.event.player.PlayerArmSwingEvent;
 import io.papermc.paper.event.player.PlayerInventorySlotChangeEvent;
-import me.sosedik.requiem.Requiem;
 import me.sosedik.requiem.feature.PossessingPlayer;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -14,7 +13,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.EntityEquipment;
@@ -72,8 +70,7 @@ public class PossessedMimikPossessor implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onHeldItemChange(@NotNull PlayerInventorySlotChangeEvent event) {
 		Player player = event.getPlayer();
-		if (event.getRawSlot() != player.getInventory().getHeldItemSlot()) return;
-
+		if (event.getSlot() != player.getInventory().getHeldItemSlot()) return;
 		if (!PossessingPlayer.isPossessing(player)) return;
 
 		LivingEntity entity = PossessingPlayer.getPossessed(player);
@@ -97,21 +94,6 @@ public class PossessedMimikPossessor implements Listener {
 		if (entityEquipment == null) return;
 
 		entityEquipment.setItem(EquipmentSlot.valueOf(event.getSlotType().name()), event.getNewItem());
-	}
-
-	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-	public void onItemDrop(@NotNull PlayerDropItemEvent event) {
-		Player player = event.getPlayer();
-		if (!PossessingPlayer.isPossessing(player)) return;
-
-		LivingEntity entity = PossessingPlayer.getPossessed(player);
-		if (entity == null) return;
-
-		EntityEquipment entityEquipment = entity.getEquipment();
-		if (entityEquipment == null) return;
-
-		entityEquipment.setItemInMainHand(null);
-		Requiem.scheduler().sync(() -> entityEquipment.setItemInMainHand(player.getInventory().getItemInMainHand()), 1L);
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
